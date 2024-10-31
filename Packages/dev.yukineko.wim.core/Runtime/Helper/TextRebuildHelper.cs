37701302da484db1148901c1797770533a437eb8
yukineko <27853966+hideki0403@users.noreply.github.com>
2024-10-31 23:54:09 +0900

    fix: BoothへのURLが正しく表示されない問題を修正

---
+++ Packages/dev.yukineko.wim.core/Runtime/Helper/TextRebuildHelper.cs
@@ -0,0 +1,22 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace yukineko.WorldIntegratedMenu
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class TextRebuildHelper : UdonSharpBehaviour
+    {
+        private void Start()
+        {
+            var text = GetComponent<Text>();
+            if (text != null)
+            {
+                text.FontTextureChanged();
+            }
+        }
+    }
+}
