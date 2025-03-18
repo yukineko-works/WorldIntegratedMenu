b07762e1d30af77ca46443cebf89c98241905924
yukineko <27853966+hideki0403@users.noreply.github.com>
2025-03-18 21:31:06 +0900

    feat: モジュール用カスタムInspectorを実装

---
+++ Packages/dev.yukineko.wim.core/Shared/ModuleInspector.cs
@@ -0,0 +1,99 @@
+using System.Collections.Generic;
+using UdonSharp;
+using UdonSharpEditor;
+using UnityEditor;
+using UnityEngine;
+
+namespace yukineko.WorldIntegratedMenu.EditorShared
+{
+    public abstract class ModuleInspector : Editor
+    {
+        protected InternalEditorI18n _i18n;
+        protected abstract string I18nUUID { get; }
+        protected virtual string[] ObjectProperties { get; }
+        protected virtual string DocumentationURL { get; }
+
+        private bool _showObjectProperties = false;
+        private bool _showUdonSharpHeader = false;
+        private bool _isUSharp = false;
+
+        protected virtual void OnEnable()
+        {
+            _i18n = new InternalEditorI18n(I18nUUID);
+            _isUSharp = target is UdonSharpBehaviour;
+        }
+
+        protected virtual void OnDisable()
+        {
+            _i18n = null;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUI.BeginChangeCheck();
+
+            EditorGUILayout.Space();
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                EditorGUILayout.LabelField(_i18n.GetTranslation("$title"), EditorStyles.largeLabel);
+                var content = EditorGUIUtility.TrIconContent("d_UnityEditor.ConsoleWindow", "Open documentation");
+                content.text = EditorI18n.GetTranslation("openDocs");
+                if (!string.IsNullOrEmpty(DocumentationURL) && GUILayout.Button(content))
+                {
+                    Application.OpenURL(DocumentationURL);
+                }
+            }
+            EditorGUILayout.Space();
+
+            if (_isUSharp && _showUdonSharpHeader)
+            {
+                using (new EditorGUILayout.VerticalScope(GUI.skin.box))
+                using (new EditorGUI.IndentLevelScope())
+                {
+                    if (UdonSharpGUI.DrawDefaultUdonSharpBehaviourHeader(target, true, false)) return;
+                }
+                EditorGUILayout.Space();
+            }
+
+            if (Application.isPlaying)
+            {
+                EditorGUILayout.HelpBox(EditorI18n.GetTranslation("playModeWarning"), MessageType.Warning);
+                return;
+            }
+
+            DrawModuleInspector();
+
+            if (ObjectProperties != null && ObjectProperties.Length > 0)
+            {
+                EditorGUILayout.Space();
+                _showObjectProperties = EditorGUILayout.Foldout(_showObjectProperties, EditorI18n.GetTranslation("internalProperties"));
+                if (_showObjectProperties)
+                {
+                    using (new EditorGUI.IndentLevelScope())
+                    {
+                        foreach (var prop in ObjectProperties)
+                        {
+                            EditorGUILayout.PropertyField(serializedObject.FindProperty(prop));
+                        }
+
+                        _showUdonSharpHeader = EditorGUILayout.Toggle("Enable U# debug", _showUdonSharpHeader);
+                    }
+                }
+            }
+
+            EditorGUILayout.Space();
+            ApplyModifiedProperties();
+        }
+
+        protected abstract void DrawModuleInspector();
+
+        protected virtual void ApplyModifiedProperties()
+        {
+            if (EditorGUI.EndChangeCheck())
+            {
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+    }
+}
